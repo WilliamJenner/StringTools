fc08a8399d2d5055c7dbc42a4885dfe5bf730487
Will Jenner <willjenner29@gmail.com>
2021-06-27 18:30:04 +0100

    MVP implementation of requirements User can input a list of strings delimited by new lines User can input a format string User can generate a list of formatted strings User can copy those to their clipboard

---
+++ Logic/FormattedStringFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    public static class FormattedStringFactory
+    {
+        public static List<string> FormatStrings(string listOfStrings, string formatString)
+        {
+            var splitList = listOfStrings.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
+
+            return FormatStrings(splitList, formatString);
+        }
+
+        public static List<string> FormatStrings(IEnumerable<string> listOfStrings, string formatString)
+        {
+            return listOfStrings.Select(str =>
+            {
+                // Remove whitespace
+                str = str.Replace(" ", string.Empty);
+                return String.Format(formatString, str);
+            }).ToList();
+        }
+    }
+}
